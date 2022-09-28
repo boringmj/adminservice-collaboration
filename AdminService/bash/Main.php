024e17ee72fc434940abe4407f7741ad158cfedf
boringmj <3239957605@qq.com>
2022-09-28 15:46:48 +0800

    构架基本结构

---
+++ AdminService/bash/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace bash;
+
+class Main {
+    /**
+     * 初始化
+     * 
+     * @return void
+     */
+    public function init() {
+        echo "hello world!";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AdminService;
+
+$autoload_path=__DIR__."/../vendor/autoload.php";
+
+# 加载composer自动加载文件
+if(is_file($autoload_path))
+    require_once $autoload_path;
+else
+    exit("Error: Unable to complete autoload!");
+
+use bash\Main;
+
+# 初始化服务
+$Main=new Main();
+$Main->init();

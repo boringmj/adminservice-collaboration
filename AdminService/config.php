a57567c357d319b680287dc4073bace3ba24df1e
boringmj <3239957605@qq.com>
2022-10-07 15:39:00 +0800

    把不小心删的东西加回去

--- AdminService/config.php
+++ AdminService/config.php
@@ -3,7 +3,30 @@
 namespace AdminService;
 
 return array(
+    // app 相关配置
+    'app'=>array(
+        'path'=>__DIR__.'/app',
+    ),
 
+    // route 相关配置
+    'route'=>array(
+        'default'=>array(
+            'app'=>'Index',
+            'controller'=>'Index',
+            'method'=>'index'
+        ),
+        'params'=>array(
+            'toget'=>array(
+                'model'=>'auto' // auto, value, list, value-list, list-value (default: auto)
+            ),
+            'rule'=>array(
+                'app'=>'/^[A-Z_][a-zA-Z0-9_\-]+$/',
+                'controller'=>'/^[A-Z_][a-zA-Z0-9_\-]+$/',
+                'method'=>'/^[a-z_][a-zA-Z0-9_\-]+$/',
+                'get'=>'/^[a-zA-Z0-9_]+$/'
+            )
+        )
+    )
 );
 
 ?>

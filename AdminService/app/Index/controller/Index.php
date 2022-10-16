410c99a6d045ab11daf8a46e12919e9e30814748
boringmj <3239957605@qq.com>
2022-10-16 12:54:49 +0800

    删除无用实例

--- AdminService/app/Index/controller/Index.php
+++ AdminService/app/Index/controller/Index.php
@@ -13,21 +13,6 @@ use AdminService\Cookie;
 class Index extends Controller {
 
     public function index() {
-        Request::setCookie('test','test1');
-        Request::addCookie('test2','test2');
-        Request::addCookie('test3','test3',60,'/admin','localhost');
-        Cookie::set('test4','test4');
-        Cookie::setByArray(array(
-            'test5'=>'test5',
-            'test6'=>array(
-                'value'=>'test6',
-                'expire'=>3600,
-                'path'=>'/admin',
-                'domain'=>'localhost',
-                'secure'=>false,
-                'httponly'=>false
-            )
-        ));
         return "Hello World!";
     }
 

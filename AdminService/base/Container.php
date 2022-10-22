56ec1ae579924e10849d9e9226288e93d7a84875
boringmj <3239957605@qq.com>
2022-10-22 17:57:45 +0800

    初步编写容器类,下班了先这样吧

--- AdminService/base/Container.php
+++ AdminService/base/Container.php
@@ -2,13 +2,148 @@
 
 namespace base;
 
+use AdminService\Exception;
+
 abstract class Container {
 
     /**
-     * 对象容器
+     * 对象实例容器
      * @var array
      */
     protected static $container;
+
+    /**
+     * 未被实例化的类容器
+     */
+    protected static $class_container;
+
+    /**
+     * 全局数据容器
+     */
+    protected static $data_container;
+
+    /**
+     * 初始化
+     * 
+     * @access public
+     * @param array $classes 需要初始化的类
+     * @return void
+     */
+    abstract static public function init(?array $classes=null): void;
+
+    /**
+     * 获取对象(如果不存在则自动实例化,自动实例化的前提是构造函数不含任何参数且在类容器中存在)
+     * 
+     * @access public
+     * @param string $name 对象名
+     * @return object
+     */
+    static public function get(string $name): object {
+        if(!isset(self::$container[$name]))
+            if(isset(self::$class_container[$name])) {
+                $class=self::$class_container[$name];
+                self::$container[$name]=new $class();
+            } else
+                throw new Exception('Object "'.$name.'" not found.');
+        return self::$container[$name];
+    }
+
+    /**
+     * 设置或添加对象
+     * 
+     * @access public
+     * @param string $name 对象名
+     * @param object $object 对象
+     * @return void
+     */
+    static public function set(string $name,object $object): void {
+        self::$container[$name]=$object;
+    }
+
+    /**
+     * 获取未被实例化的类名称
+     * 
+     * @access public
+     * @param string $name 类名
+     */
+    static public function getClass(string $name): string {
+        // 如果类容器中不存在该类则抛出异常
+        if(!isset(self::$class_container[$name]))
+            throw new Exception('Class "'.$name.'" not found.');
+        return self::$class_container[$name];
+    }
+
+    /**
+     * 设置或添加未被实例化的类
+     * 
+     * @access public
+     * @param string $name 类名
+     * @param string $class 类
+     * @return void
+     */
+    static public function setClass(string $name,string $class): void {
+        self::$class_container[$name]=$class;
+    }
+
+    /**
+     * 批量设置或添加对象
+     * 
+     * @access public
+     * @param array $objects 对象数组
+     * @return void
+     */
+    static public function setByArray(array $objects): void {
+        foreach($objects as $name=>$object)
+            self::set($name,$object);
+    }
+
+    /**
+     * 批量设置或添加未被实例化的类
+     * 
+     * @access public
+     * @param array $classes 类数组
+     * @return void
+     */
+    static public function setClassByArray(array $classes): void {
+        foreach($classes as $name=>$class)
+            self::setClass($name,$class);
+    }
+
+    /**
+     * 获取全局数据
+     * 
+     * @access public
+     * @param string $name 数据名
+     * @param mixed $default 默认值
+     * @return mixed
+     */
+    static public function getData(string $name,mixed $default=null): mixed {
+        return self::$data_container[$name]??$default;
+    }
+
+    /**
+     * 设置或添加全局数据
+     * 
+     * @access public
+     * @param string $name 数据名
+     * @param mixed $data 数据
+     * @return void
+     */
+    static public function setData(string $name,mixed $data): void {
+        self::$data_container[$name]=$data;
+    }
+
+    /**
+     * 批量设置或添加全局数据
+     * 
+     * @access public
+     * @param array $datas 数据数组
+     * @return void
+     */
+    static public function setDataByArray(array $datas): void {
+        foreach($datas as $name=>$data)
+            self::setData($name,$data);
+    }
 
 }
 
---
+++ AdminService/core/App.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AdminService;
+
+use base\Container;
+
+final class APP extends Container {
+
+    /**
+     * 初始化
+     * 
+     * @access public
+     * @param array $classes 需要初始化的类
+     * @return void
+     */
+    static public function init(?array $classes=null): void {
+        if($classes===null)
+            $classes=array();
+        $classes=array(
+            'Router'=>$classes['Router']??Router::class,,
+            'View'=>$classes['View']??View::class,
+            'Controller'=>$classes['Controller']??Controller::class,
+            'Model'=>$classes['Model']??Model::class,
+            'Request'=>$classes['Request']??Request::class,
+            'File'=>$classes['File']??File::class,
+            'Exception'=>$classes['Exception']??Exception::class,
+            'Cookie'=>$classes['Cookie']??Cookie::class,
+        );
+        parent::$class_container=$classes;
+    }
+
+    /**
+     * 获取当前应用名称
+     * 
+     * @access public
+     * @return string
+     */
+    static public function getAppName(): string {
+        self::initRouteInfo();
+        if(self::getData('route_info')!==null)
+            return self::getData('route_info')['app']??null;
+        return null;
+    }
+
+    /**
+     * 获取当前控制器名称
+     */
+    static public function getControllerName(): string {
+        self::initRouteInfo();
+        if(self::getData('route_info')!==null)
+            return self::getData('route_info')['controller']??null;
+        return null;
+    }
+
+    /**
+     * 获取当前方法名称
+     */
+    static public function getMethodName(): string {
+        self::initRouteInfo();
+        if(self::getData('route_info')!==null)
+            return self::getData('route_info')['method']??null;
+        return null;
+    }
+
+    /**
+     * 初始化路由信息
+     * 
+     * @access private
+     * @return void
+     */
+    static private function initRouteInfo(): void {
+        // 检查是否存在缓存
+        if(self::getData('route_info')===null) {
+            // 获取路由信息
+            $route_info=parent::get('Router')->getRouteInfo();
+            // 缓存路由信息
+            parent::setData('route_info',$route_info);
+        }
+    }
+}
+
+?>

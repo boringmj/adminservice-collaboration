8b38ec27cef922b90a5bee415682949795956e5a
boringmj <3239957605@qq.com>
2022-11-03 17:07:29 +0800

    优化模型类的使用,更新实例

--- AdminService/app/Index/model/Sql.php
+++ AdminService/app/Index/model/Sql.php
@@ -6,10 +6,16 @@ use base\Model;
 
 class Sql extends Model {
 
+    public string $table_name='admin_service_system_info'; // 完整表名(含前缀)
+
     public function test() {
-        return $this->table('system_info')->where('app_id','thisisdemo')->select();
+        return $this->where('app_id','thisdemo')->select(array('id','app_key'));
     }
 
+    public function demo() {
+        // 注意, 当这里传递入表名后, 后续请求的表名将会被覆盖
+        return $this->table('system_info')->where('app_id','thisdemo')->select(array('id','app_key'));
+    }
 }
 
 ?>
--- AdminService/base/Model.php
+++ AdminService/base/Model.php
@@ -14,6 +14,31 @@ use base\Database;
  */
 abstract class Model extends Database {
 
+    /**
+     * 查询数据
+     * 
+     * @access public
+     * @param string|array $fields 查询字段(默认为*)
+     * @return mixed
+     */
+    public function select(string|array $fields='*'): mixed {
+        return $this->db_object->select();
+    }
+
+    /**
+     * 根据条件查询数据
+     * 
+     * @access public
+     * @param string|array $where 字段名称或者数据数组
+     * @param mixed $data 查询数据
+     * @param string $operator 操作符
+     * @return self
+     */
+    public function where(string|array $where,mixed $data=null,?string $operator='='): self {
+        $this->db_object->where($where,$data,$operator);
+        return $this;
+    }
+
 }
 
 ?>

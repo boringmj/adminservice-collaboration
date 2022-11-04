fb152eaf0a63deb406debdcdd2d06f837e4833df
boringmj <3239957605@qq.com>
2022-11-04 15:25:59 +0800

    UUID现在支持使用时间戳

--- AdminService/common/uuid.php
+++ AdminService/common/uuid.php
@@ -5,16 +5,21 @@ namespace AdminService\common;
 /**
  * 生成UUID
  * 
+ * @param string $use_time 是否使用时间戳
  * @return string
  */
-function uuid(): string {
+function uuid($use_time=false): string {
     $charid=strtoupper(md5(uniqid(mt_rand(),true)));
+    $server_time=dechex(time());
+    $server_time=str_pad(substr($server_time,-8),8,'0',STR_PAD_LEFT);
     $hyphen=chr(45);
-    $uuid=substr($charid, 0, 8).$hyphen
+    $uuid=($use_time?$server_time:substr($charid,0,8)).$hyphen
         .substr($charid, 8, 4).$hyphen
         .substr($charid,12, 4).$hyphen
         .substr($charid,16, 4).$hyphen
         .substr($charid,20,12);
+    // 转为全小写
+    $uuid=strtolower($uuid);
     return $uuid;
 }
 

cbe8ab277823d5b9b41c3963b46f4aa1dfa121ff
boringmj <3239957605@qq.com>
2022-11-05 16:12:45 +0800

    重命名-完成

---
+++ AdminService/app/index/model/Count.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\index\model;
+
+use base\Model;
+use AdminService\File;
+use AdminService\Exception;
+
+class Count extends Model {
+
+    /**
+     * 文件对象
+     */
+    private File $file;
+
+    /**
+     * 构造方法
+     * 
+     * @access public
+     * @param File $file 文件对象
+     */
+    public function __construct(File $file=(new File('count'))) {
+        $this->file=$file;       
+    }
+
+    /**
+     * 添加计数器
+     * 
+     * @access public
+     * @return int
+     */
+    public function add(): int {
+        try {
+            $file=$this->file;
+            $count=$file->get('count',0);
+            $file->set('count',$count+1,true);
+            return $file->get('count',0);
+        } catch(Exception) {
+            return -1;
+        }
+    }
+
+}
+
+?>

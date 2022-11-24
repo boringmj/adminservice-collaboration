7915bc5c8c6f5e931c06cd31678e2829b3c1fba4
boringmj <2878663643@qq.com>
2022-11-24 16:05:34 +0800

    在配置文件中引入该函数

--- AdminService/config.php
+++ AdminService/config.php
@@ -51,7 +51,8 @@ return array(
         'loader'=>array( // 需要自动加载的函数文件名(不含扩展名)
             'uuid', // uuid函数
             'helper', // 通用助手函数
-            'controller_helper' // 控制器助手函数
+            'controller_helper', // 控制器助手函数
+            'sign' // 签名函数
         )
     ),
 

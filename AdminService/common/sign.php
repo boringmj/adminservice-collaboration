29bda78c19b64cbcb78c4e2e5d3dbfe206c72bc3
boringmj <2878663643@qq.com>
2022-11-24 16:06:47 +0800

    修复一个注释错误

--- AdminService/common/sign.php
+++ AdminService/common/sign.php
@@ -6,7 +6,6 @@ namespace AdminService\common;
  * 签名(先按键名排序, 逐一按 "key=value" 用 "&" 拼接, 最后进行MD5)
  * 
  * @param array $data 需要签名的数据
- * @param string $app_key 签名的密钥
  * @return string
  */
 function sign($data)

4962f1d9ff3187ffb2bca248c28f873d49bfa652
boringmj <2878663643@qq.com>
2022-11-24 16:04:05 +0800

    新增签名

---
+++ AdminService/common/sign.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AdminService\common;
+
+/**
+ * 签名(先按键名排序, 逐一按 "key=value" 用 "&" 拼接, 最后进行MD5)
+ * 
+ * @param array $data 需要签名的数据
+ * @param string $app_key 签名的密钥
+ * @return string
+ */
+function sign($data)
+{
+    krsort($data);
+    $sign_string='';
+    foreach($data as $key=>$value)
+        $sign_string.=(empty($sign_string)?'':'&')."{$key}={$value}";
+    return md5($sign_string);
+}
+
+?>

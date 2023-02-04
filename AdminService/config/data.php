adcffcd83f63498602ed61e54ed8f253d33b3f1e
boringmj <3239957605@qq.com>
2023-02-04 13:31:45 +0800

    修改两条配置描述

--- AdminService/config/data.php
+++ AdminService/config/data.php
@@ -4,7 +4,7 @@ namespace AdminService\config;
 
 // data 相关配置
 return array(
-    'path'=>__DIR__.'/../data', // 该目录需要可写权限
+    'path'=>__DIR__.'/../data', // 该目录需要可写权限, 如果目录不存在, 则请赋予其父目录可写权限, 系统会自动创建该目录
     'ext_name'=>'.data.json', // 文件扩展名
     'dir_mode'=>0644, // 自动创建的目录权限(Windows下无效)
     'rule'=>array(
--- AdminService/config/log.php
+++ AdminService/config/log.php
@@ -4,7 +4,7 @@ namespace AdminService\config;
 
 // log 相关配置
 return array(
-    'path'=>__DIR__.'/../log', // 该文件需要可写权限
+    'path'=>__DIR__.'/../log', // 该目录需要可写权限, 如果目录不存在, 则请赋予其父目录可写权限, 系统会自动创建该目录
     'ext_name'=>'.log', // 文件扩展名
     'dir_mode'=>0644, // 自动创建的目录权限(Windows下无效)
     'max_size'=>104857600, // 单个日志文件最大尺寸(单位: 字节),默认100M (default: 104857600)

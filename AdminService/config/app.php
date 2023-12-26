c64b4904942d8c758fa37a267834a19b66b9c867
boringmj <2878663643@qq.com>
2023-12-26 20:08:11 +0800

    修改注释,以防止分歧出现

--- AdminService/config/app.php
+++ AdminService/config/app.php
@@ -5,7 +5,7 @@ namespace AdminService\config;
 // app 相关配置
 return array(
     'path'=>__DIR__.'/../app', // app 目录
-    'classes'=>array( // 需要初始化的类,这些类会自动绑定到容器中
+    'classes'=>array( // 需要初始化的类,这些类会自动绑定到容器中(这里就相当于一个别名,而且对象是懒加载的)
         'Router'=>\AdminService\Router::class,
         'View'=>\AdminService\View::class,
         'Request'=>\AdminService\Request::class,

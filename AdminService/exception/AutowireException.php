bfaf5f2fa67a84db20b0d8df2924e9554c246172
boringmj <3239957605@qq.com>
2025-10-06 23:40:02 +0800

    feat(exception): 添加数据库连接异常类及自动注入异常类文档注释

--- AdminService/exception/AutowireException.php
+++ AdminService/exception/AutowireException.php
@@ -4,4 +4,7 @@ namespace AdminService\exception;
 
 use AdminService\Exception;
 
+/**
+ * 自动注入异常类
+ */
 class AutowireException extends Exception { }
---
+++ AdminService/exception/sql/ConnectionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AdminService\exception\sql;
+
+use AdminService\Exception;
+
+/**
+ * 数据库连接异常类
+ */
+class ConnectionException extends Exception { }
